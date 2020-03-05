7226a20f9ec334fbf45d731254d9df7bc49fbcaf
Junior Cardoso <50811299+ElementaryBR@users.noreply.github.com>
2020-03-05 09:16:08 -0300

    up

--- Aula01/Exercicio03.py
+++ Aula01/Exercicio03.py
@@ -23,19 +23,76 @@
 # crite testes para validar cada uma destas classes!
 
 import math
+import random
 
 class Circulo:
     def __init__(self):
-        self.cor = ''
-        self.raio = ''
-        self.material = ''
+        self.__cor = ''
+        self.__raio = 0.0
+        self.__material = ''
+
+    def getter_raio(self):
+        return self.__raio
+
+    def getter_cor(self):
+        return self.__cor
+
+    def lista_de_cores(self):
+        return ['Rosa','Amarelo','Preto','Vermelho','Azul','Verde','Cinza','Roxo','Prata','Branco','Prata','Dourado','Caramelo','Marfim','Marrom']
+
+    def setter_raio(self,raio):
+        self.__raio = self.area(raio)
 
     def trocaCor(self):
-        pass
+        self.cor_aleatoria = random.choice(self.lista_de_cores())
+        self.__cor = self.cor_aleatoria
+
     def mostraCor(self):
-        pass
+        return self.__cor
+
     def area(self,raio):
         result = (math.pi*raio**2)
         return result
-class Quadrado:
 
+class Quadrado:
+    def __init__(self):
+        self.__tamanho_do_lado = 0
+
+    def getter(self):
+        return self.__tamanho_do_lado
+
+    def mudar_tamanho(self,valor):
+        self.mudar_do_lado = valor
+
+    def calcular(self):
+        self.resultado = self.mudar_do_lado * self.mudar_do_lado
+        self.mudar_tamanho(self.resultado)
+        return self.resultado
+
+class Retangulo:
+    def __init__(self):
+        self.__base = 0
+        self.__altura = 0
+
+    def setter_lados(self,base,altura):
+        self.__altura = altura
+        self.__base = base
+
+    def getter(self):
+        base = self.__base
+        altura = self.__altura
+        return f'A base tem valor de {base} e a altura tem valor de {altura}'
+
+    def calculo_area(self):
+        result = self.__base * self.__altura
+        return result
+
+circulo = Circulo()
+quadrado = Quadrado()
+retangulo = Retangulo()
+
+
+circulo.trocaCor()
+print(circulo.getter_cor())
+circulo.setter_raio(5)
+print(f'{circulo.getter_raio():.2f}')
